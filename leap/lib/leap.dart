78f1fea34f4931acfe64396b68b150899d43f862
Caroline Chan <carolinechan304@gmail.com>
2023-07-25 08:17:15 +0700

    leap year

---
+++ leap/lib/leap.dart
@@ -0,0 +1,18 @@
+class Leap {
+  // Put your code here
+  bool leapYear(int year) {
+    if (year % 4 == 0) {
+      if (year % 100 == 0) {
+        if (year % 400 == 0) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  }
+}
